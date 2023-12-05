62d724d34c32a3c6a7650c962482f3cae082528a
zamarz <zachmarzouk@gmail.com>
2023-12-05 11:04:34 +0000

    Change styling

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '6d8a721bf4dd8e7c1452');
--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 
 function portfolio_files() {
-    wp_enqueue_style('portfolio_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('portfolio_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('portfolio_extra_styles', get_theme_file_uri('/build/index.css'));
+
 }
 
 add_action('wp_enqueue_scripts', 'portfolio_files');
