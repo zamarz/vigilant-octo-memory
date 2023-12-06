72c5582d8ac92d4f5e737f5ddc14f7ee3c9cb7da
zamarz <zachmarzouk@gmail.com>
2023-12-06 10:17:07 +0000

    Update some links in header

--- header.php
+++ header.php
@@ -7,14 +7,14 @@
     <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
-          <a href="#"><strong>Zach</strong> Marzouk</a>
+          <a href="<?php echo site_url('/'); ?>"><strong>Zach</strong> Marzouk</a>
         </h1>
         <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
             <ul>
-              <li><a href="#">About Me</a></li>
+              <li><a href="<?php echo site_url('/about-me'); ?>">About Me</a></li>
               <li><a href="#">Projects</a></li>
               <li><a href="#">Tech Stack</a></li>
               <li><a href="#">My Articles</a></li>
