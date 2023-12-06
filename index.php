85452d6b9122ff407905fffb088d5f010f0ef881
zamarz <zachmarzouk@gmail.com>
2023-12-06 14:52:13 +0000

    Add more style to single.php

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-This is the generic blog listing screen template.
+<?php 
+get_header();
+?>
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg');?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">My Projects</h1>
+        <div class="page-banner__intro">
+          <p>Some of the stuff I've been working on</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+      <?php 
+      while(have_posts()) {
+        the_post(); ?>
+        <div class="post-item">
+          <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+            <div class="metabox">
+              <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?></p>
+
+            </div>
+            <div class="generic-content">
+              <?php the_excerpt(); ?>
+              <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">See project &raquo;</a></p>
+
+            </div>
+        </div>
+
+      <?php }
+      
+      ?>
+    </div>
+
+
+<?php
+get_footer();
+
+?>
