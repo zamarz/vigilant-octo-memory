03e2de85bb499e4738c5c221f12b7ae2e65c475a
zamarz <zachmarzouk@gmail.com>
2024-01-08 15:42:28 +0000

    Customise tech stack page

--- page.php
+++ page.php
@@ -9,7 +9,7 @@ while(have_posts()) {
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
-          <p>CHANGE THIS - ADD CUSTOM FIELD</p>
+          <p>This is some of the tech I work with</p>
         </div>
       </div>
     </div>
@@ -17,17 +17,9 @@ while(have_posts()) {
     <div class="container container--narrow page-section">
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href="<?php echo site_url('/'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Home</a>
         </p>
       </div>
-
-      <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
-        <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
-        </ul>
-      </div> -->
 
       <div class="generic-content">
         <?php the_content(); ?>
