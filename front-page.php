40f09225663d114dd64c24c6bd7852c7e562c354
zamarz <zachmarzouk@gmail.com>
2024-01-09 15:34:37 +0000

    Change front page links

--- front-page.php
+++ front-page.php
@@ -25,9 +25,9 @@
                 $homePagePosts->the_post(); ?>
            
            <div class="event-summary">
-            <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-              <span class="event-summary__month"><?php the_time('M'); ?></span>
-              <span class="event-summary__day"><?php the_time('d'); ?></span>
+            <a  href="<?php the_permalink(); ?>">
+
+              <?php the_post_thumbnail(); ?>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
