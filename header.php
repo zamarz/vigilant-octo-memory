4ae866252ffc97624e5f704f6b738e5c15e24de5
zamarz <zachmarzouk@gmail.com>
2024-01-10 14:27:04 +0000

    Minor changes to header

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
         <h1 class="school-logo-text float-left">
           <a href="<?php echo site_url('/'); ?>"><strong>Zach</strong> Marzouk</a>
         </h1>
-        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+    
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
